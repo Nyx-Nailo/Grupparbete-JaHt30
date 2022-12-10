0c03b69be4de3aff8f4f0e341eb708fbfb244d14
izuvell1 <isaiasprio1@gmail.com>
2022-12-10 22:29:57 +0100

    Om Oss
    om oss, framtida ambitioner, kontakt information och socials.

---
+++ car-pool/src/Pages/OmOss.js
@@ -0,0 +1,53 @@
+function OmOss() {
+    return (
+        <>
+        <div id="container">
+        <div id="flex-item1">
+        <h2>Om Oss</h2> 
+        <br></br>
+        <p>Ln, sagittis et risus. Aliquam vel risus congue, mollis lorem ut, commodo ante. Vestibulum aliquam felis eros, id accumsan nulla rhoncus at. Nulla lacus eros, vehicula sed dapibus ut, auctor in massa. Morbi blandit, risus quis porta tempor, massa purus placerat turpis, vel sollicitudin nisi sapien at ante. Aliquam suscipit mi enim. Curabitur id mauris eu libero pellentesque pulvinar eu id sapien. Aenean vel sodales urna, at faucibus metus. Aliquam finibus, odio at maximus rutrum, justo purus volutpat risus, non varius justo dolor vitae diam. Donec augue leo, posuere vitae laoreet sed, sollicitudin non urna. Fusce dignissim sollicitudin ex, eget scelerisque sem mattis a. In turpis metus, venenatis eu ante a, porta posuere diam. Mauris tempus ornare luctus. Ut gravida nulla in est laoreet, sit amet aliquet velit molestie.</p></div>
+        <div id="flex-item2">
+        <h2>Våra Ambitioner För Framtiden</h2> 
+        <br></br>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed tellus pellentesque, mollis velit ut, condimentum tortor. Quisque maximus condimentum eros, quis semper nisl vehicula tincidunt. Cras blandit, urna id aliquam sodales, purus erat maximus tortor, sed gravida neque quam ac lorem. Sed vitae justo viverra, interdum sem et, aliquam est. Pellentesque velit tellus, tempus non commodo non, pharetra vel dui. Donec eget est sed ipsum commodo commodo sit amet non nibh. Nulla id viverra urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed nec iaculis lectus. Curabitur vulputate leo ac ante rutrum, vitae accumsan ligula aliquet. Vestibulum pulvinar, nisl et faucibus eleifend, ante justo elementum tortor, sit amet vulputate nisi nulla tristique risus. Mauris leo tellus, sagittis nec ante at, fermentum facilisis turpis.
+        </p>
+        </div>
+
+        <div id="flex-item3">
+        <h2>Kontakter</h2>
+
+        <br></br>
+
+        <div id="container">   
+        <div id="bilden">
+        <img width="15%" id="img"src="../lur.png" alt="fb"></img>
+        </div>
+        <div id="nummer">
+        <p>+46-12345678</p>
+        </div>
+        </div>
+
+        <br></br>
+
+        <div id="container2">   
+        <div id="bilden2">
+        <img width="15%" id="img"src="../email.png" alt="fb"></img>
+        </div>
+        <div id="Email">
+        <p>CarPoolSE@gmail.com</p>
+        </div>
+        </div>
+        <br></br>
+        <div id="Links">
+           <a href="https://www.instagram.com/" ><img width ="5%" src="../insta.png"></img></a>
+           <a href="facebook.com" ><img width ="5%" src="../fb.png"></img></a>
+           <a href="https://se.linkedin.com/" ><img width ="5%" src="../link.png"></img></a>
+        </div>
+        <br></br>
+        </div>
+        </div>
+        </>
+        );
+}
+export default OmOss;
